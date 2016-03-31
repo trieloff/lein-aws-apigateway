9944fec6b9ad56fe24bfcabcef5ea870028a0d1f
Lars Trieloff <lars@trieloff.net>
2016-03-31 18:58:54 +0200

    Cleaned up dependencies

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen false
   :dependencies [[com.amazonaws/aws-apigateway-importer "1.0.3-SNAPSHOT" :exclusions [joda-time com.amazonaws/aws-java-sdk-core]]
-                 [com.amazonaws/aws-java-sdk "1.10.62"]])
+                 [com.amazonaws/aws-java-sdk-core "1.9.40" :exclusions [joda-time]]])
