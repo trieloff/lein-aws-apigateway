85f62bbbfbf1a2288deba222f95b93c2228e986e
Lars Trieloff <lars@trieloff.net>
2016-04-17 16:22:24 +0200

    Support updates

--- src/leiningen/aws_api_gateway.clj
+++ src/leiningen/aws_api_gateway.clj
@@ -1,7 +1,8 @@
 (ns leiningen.aws-api-gateway
   "Deploy swagger.json to API Gateway"
   (import com.amazonaws.services.apigateway.AmazonApiGatewayClient
-          com.amazonaws.services.apigateway.model.ImportRestApiRequest)
+          com.amazonaws.services.apigateway.model.ImportRestApiRequest
+          com.amazonaws.services.apigateway.model.PutRestApiRequest)
   (require [clojure.pprint :refer [pprint]]
            [byte-streams :as byte-streams]
            [amazonica.aws.apigateway :as aws]
@@ -53,6 +54,23 @@
       (.setParameters request {})
       (.getId (.importRestApi (AmazonApiGatewayClient.) request)))))
 
+(defn update-rest-api [swagger id]
+  (let [swaggerbb (byte-streams/convert swagger java.nio.ByteBuffer)
+        request (.withMode
+                  (.withRestApiId
+                    (.withBody
+                      (PutRestApiRequest.)
+                      swaggerbb)
+                    id)
+                  "overwrite")]
+    (do
+      (.setParameters request {})
+      (.getId (.putRestApi (AmazonApiGatewayClient.) request)))))
+
 (def myfile (clojure.java.io/file "/Users/trieloff/Documents/excelsior/resources/swagger-example.json"))
 
-(import-rest-api myfile)
+;(import-rest-api myfile)
+
+(aws/get-rest-api :rest-api-id "04511k2k4e")
+
+(update-rest-api myfile "04511k2k4e")
